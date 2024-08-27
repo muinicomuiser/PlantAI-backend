37453f692a3d0379249d593dd5776c9f2dd2ee1c
Nicolas <fernandez.ro.nicolas@gmail.com>
2024-08-26 21:21:22 -0400

    edicion dto carrocompra

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,11 +4,4 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-  @ApiTags('Saludo')
-  @ApiOperation({summary: 'Saludo de entrada.'})
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
