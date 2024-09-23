3780f4da6462e16b9495965244a5edcb3758d9b2
muinicomuiser <nicodoneg@gmail.com>
2024-09-23 00:58:54 -0300

    Primer commit productos_create

--- src/productos/dto/create-producto.dto.ts
+++ src/productos/dto/create-producto.dto.ts
@@ -1,1 +1,16 @@
-export class CreateProductoDto {}
+import { FotoPeriodo, TipoRiego } from "../entities/categorias";
+
+export class CreateProductoDto {
+    nombre: string;
+    precio: number;
+    imagen: string;     //URL
+    descripcion: string;
+    cantidad: number    //Stock
+    unidadesVendidas: number;
+    puntuacion: number;
+    familia: string;            //<-- enum familia
+    fotoperiodo: FotoPeriodo;   //<-- enum FotoPeriodo
+    tipoRiego: TipoRiego;       //<-- enum TipoRiego
+    petFriendly: boolean;
+    color: string;
+}
