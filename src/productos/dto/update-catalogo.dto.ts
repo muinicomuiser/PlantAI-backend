f4a24c63e1d8a00aa1b96eab2879b42e74661f46
Nicolas <fernandez.ro.nicolas@gmail.com>
2024-10-06 17:12:56 -0300

    delete catalogo dto

--- src/productos/dto/update-catalogo.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCatalogoDto } from './create-catalogo.dto';
-
-export class UpdateCatalogoDto extends PartialType(CreateCatalogoDto) {}
