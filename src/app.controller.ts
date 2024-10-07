686429d1554a1dd2a2fb4510fcb234fc8b30af8f
Nicolas <fernandez.ro.nicolas@gmail.com>
2024-10-07 17:43:59 -0300

    comentario eliminado

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
 
-// @ApiTags('testeo ExceptionFilter')
 @Controller()
-export class AppController {
-  // @ApiResponse({ status: 400, description: 'Mensaje de prueba' })
-  // @Get('exception')
-  // throwException() {
-  //   throw new HttpException('Mensaje de prueba', HttpStatus.BAD_REQUEST);
-  // }
-}
+export class AppController {}
