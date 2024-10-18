34c5dbb26b0a43f8d9510ef4c01bb467eec60ae6
muinicomuiser <nicodoneg@gmail.com>
2024-10-18 17:55:39 -0300

    Entidades decoradas con Entity

--- src/productos/entities/plantas/habito_crecimiento.entity.ts
+++ src/productos/entities/plantas/habito_crecimiento.entity.ts
@@ -1,6 +1,6 @@
 import { Entity } from "typeorm";
 
-@Entity({ name: 'plantas_habuto_crecimiento' })
+@Entity({ name: 'plantas_habito_crecimiento' })
 export class HabitoCrecimiento {
   id: number;
   crecimiento: string;
