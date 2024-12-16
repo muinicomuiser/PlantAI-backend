462978b7aaace2f87b880d59901db6e40d534e9d
NicolasFernandezR <fernandez.ro.nicolas@gmail.com>
2024-12-16 20:32:01 -0300

    feat: search by string

--- src/productos/service/catalogo.service.ts
+++ src/productos/service/catalogo.service.ts
@@ -1,7 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { FiltrosCatalogoDto } from '../dto/catalogo/paginacion.dto';
+import {
+  FiltrosCatalogoDto,
+  SearchCatalogoDto,
+} from '../dto/catalogo/paginacion.dto';
 import { Producto } from '../entities/producto.entity';
 import { ProductoMapper } from '../mapper/entity-to-dto-producto';
 
@@ -10,7 +13,7 @@ export class CatalogoService {
   constructor(
     @InjectRepository(Producto)
     private readonly productoRepository: Repository<Producto>,
-  ) { }
+  ) {}
 
   /**Retorna todos los productos */
   async findAll(filtrosCatalogoDto: FiltrosCatalogoDto) {
@@ -92,6 +95,46 @@ export class CatalogoService {
         queryBuilder.orderBy(`producto.${ordenarPor}`, orden || 'ASC');
       }
       // console.log(ordenarPor, orden);
+
+      queryBuilder.skip(offset).take(limit);
+
+      const [result, totalItems] = await queryBuilder.getManyAndCount();
+      const productos = result.map((producto) =>
+        ProductoMapper.entityToDto(producto),
+      );
+
+      return { data: productos, totalItems };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  // retorna producto por busqueda
+
+  async findBySearch(searchCatalogoDto: SearchCatalogoDto) {
+    try {
+      const { page, pageSize, search } = searchCatalogoDto;
+      const limit = pageSize;
+      const offset = (page - 1) * limit;
+      const queryBuilder = this.productoRepository
+        .createQueryBuilder('producto')
+        .innerJoinAndSelect('producto.categoria', 'categoria')
+        .leftJoinAndSelect('producto.planta', 'planta')
+        .leftJoinAndSelect('planta.entorno', 'entorno')
+        .leftJoinAndSelect('producto.imagenes', 'imagenes')
+        .leftJoinAndSelect('planta.iluminacion', 'iluminacion')
+        .leftJoinAndSelect('planta.tipoRiego', 'tipoRiego')
+        .leftJoinAndSelect(
+          'planta.toleranciaTemperatura',
+          'toleranciaTemperatura',
+        )
+        .leftJoinAndSelect('planta.color', 'color')
+        .leftJoinAndSelect('planta.fotoPeriodo', 'fotoPeriodo')
+        .leftJoinAndSelect('planta.habitoCrecimiento', 'habitoCrecimiento')
+        .leftJoinAndSelect('planta.tamano', 'tamano')
+        .where('producto.habilitado = :habilitado', { habilitado: true })
+        .andWhere('producto.idCategoria = :idCategoria', { idCategoria: 1 })
+        .andWhere('producto.nombre LIKE :search', { search: `%${search}%` });
 
       queryBuilder.skip(offset).take(limit);
 
