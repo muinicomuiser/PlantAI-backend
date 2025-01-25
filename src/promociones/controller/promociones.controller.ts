9c2cbe49e65536ce493f54624760f819d479a9a5
muinicomuiser <nicodoneg@gmail.com>
2025-01-25 19:20:37 -0300

    Descripciones documentación promociones

--- src/promociones/controller/promociones.controller.ts
+++ src/promociones/controller/promociones.controller.ts
@@ -14,8 +14,11 @@ import { GetCuponValidadoDto } from '../dto/get_cupon_validado.dto';
 export class PromocionesController {
     constructor(private readonly promocionesService: PromocionesService) { }
 
+    // Obtener todas las promociones
     @ApiOperation({
-        summary: 'Obtener todas las promociones activas'
+        summary: 'Obtener todas las promociones activas',
+        description: `Retorna todas las promociones habilitadas y para las que la fecha actual `
+            + `esté entre la fecha de inicio y la de término de la promoción.`
     })
     @ApiResponse({
         status: 200,
@@ -29,7 +32,10 @@ export class PromocionesController {
 
     // Obtener productos por promoción
     @ApiOperation({
-        summary: 'Obtener los productos paginados de una promoción'
+        summary: 'Obtener los productos paginados de una promoción',
+        description: `Retorna todos los productos seleccionados de una promoción `
+            + `cuya propiedad "tipoSeleccionProductos" sea "SELECCIONADOS".\n`
+            + `Permite definir paginación.`
     })
     @ApiResponse({
         status: 200,
@@ -59,7 +65,12 @@ export class PromocionesController {
 
     // Validar cupón
     @ApiOperation({
-        summary: 'Validar código de cupón'
+        summary: 'Validar código de cupón',
+        description: `Recibe un código requerido para validar un cupón.\n`
+            + `\nSi se encuentra un cupón para ese código, se retorna una respuesta `
+            + `positiva ("validado": true), junto con el id del cupón y el código usado para validar.\n`
+            + `\nSi no se encuentra un cupón que coincida con el código,`
+            + ` se retorna una respuesta negativa ("validado": false).\n`
     })
     @ApiResponse({
         status: 200,
@@ -70,8 +81,8 @@ export class PromocionesController {
         status: 400,
         description: 'Error al comprobar código'
     })
-    @Get('coupon/:promoCode')
-    async validateCoupon(@Param('promoCode') code: string): Promise<GetCuponValidadoDto> {
+    @Get('coupon/:codigoPromocion')
+    async validateCoupon(@Param('codigoPromocion') code: string): Promise<GetCuponValidadoDto> {
         return await this.promocionesService.validateCoupon(code)
     }
 
@@ -97,7 +108,18 @@ export class PromocionesController {
 
     // Crear promoción
     @ApiOperation({
-        summary: 'Crear promoción'
+        summary: 'Crear promoción',
+        description: `Propiedades: \n`
+            + `- codigo: Palabra requerida para activar un cupón (solo aplica a cupones)\n`
+            + `- valor: Descuento o precio final del producto, dependiendo si la promoción es por porcentaje o precio fijo.\n`
+            + `- idTipoPromocion: 1: TRADICIONAL, 2: CUPON -> TRADICIONAL para descuentos `
+            + `que se aplican por defecto al producto, CUPON para descuentos que requieren `
+            + `ingresar un código para activarlos.\n`
+            + `- idTipoDescuento: 1: PORCENTAJE, 2: FIJO -> PORCENTAJE cuando la propiedad `
+            + `"valor" define el porcentaje que se descuenta, FIJO cuando define el precio final del producto.\n`
+            + `- idTipoSeleccionProductos: 1: TODOS, 2: SELECCIONADOS -> Si la promoción se `
+            + `aplica a todos los productos o a productos seleccionados.\n`
+            + `- idsProductos: Arreglo de id de los productos seleccinados de una promoción.\n`
     })
     @ApiResponse({
         status: 201,
@@ -118,7 +140,19 @@ export class PromocionesController {
 
     // Modificar promoción
     @ApiOperation({
-        summary: 'Modificar una promoción'
+        summary: 'Modificar una promoción',
+        description: `Propiedades: \n`
+            + `- codigo: Palabra requerida para activar un cupón (solo aplica a cupones)\n`
+            + `- valor: Descuento o precio final del producto, dependiendo si la promoción es por porcentaje o precio fijo.\n`
+            + `- idTipoPromocion: 1: TRADICIONAL, 2: CUPON -> TRADICIONAL para descuentos `
+            + `que se aplican por defecto al producto, CUPON para descuentos que requieren `
+            + `ingresar un código para activarlos.\n`
+            + `- idTipoDescuento: 1: PORCENTAJE, 2: FIJO -> PORCENTAJE cuando la propiedad `
+            + `"valor" define el porcentaje que se descuenta, FIJO cuando define el precio final del producto.\n`
+            + `- idTipoSeleccionProductos: 1: TODOS, 2: SELECCIONADOS -> Si la promoción se `
+            + `aplica a todos los productos o a productos seleccionados.\n`
+            + `- productosModificados: Acciones sobre el conjunto de productos seleccionados de la promoción. `
+            + `Permite agregar o remover productos asignados a una promoción.\n`
     })
     @ApiResponse({
         status: 200,
--- src/promociones/dto/get_cupon_validado.dto.ts
+++ src/promociones/dto/get_cupon_validado.dto.ts
@@ -2,13 +2,21 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class GetCuponValidadoDto {
 
-    @ApiProperty()
-    idCupon?: number;
-
-    @ApiProperty()
+    @ApiProperty({
+        description: 'Define si se ha validado un cupón o no'
+    })
     validado: boolean;
 
-    @ApiProperty()
+    @ApiProperty({
+        description: 'ID del cupón validado con el código',
+        required: false
+    })
+    idCupon?: number;
+
+    @ApiProperty({
+        description: 'Código usado para validar el cupón',
+        required: false
+    })
     codigoValidacion?: string;
 
 }
